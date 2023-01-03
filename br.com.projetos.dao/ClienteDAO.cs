0e468a7f8d36d4eb9dc2b72310bce88a951f0475
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-01-03 06:41:55 -0300

    Criação do método de CadastrarCleinte

--- br.com.projetos.dao/ClienteDAO.cs
+++ br.com.projetos.dao/ClienteDAO.cs
@@ -1,13 +1,67 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using ProjetosControle_De_Vendas.br.com.projetos.conexao;
+using ProjetosControle_De_Vendas.br.com.projetos.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ProjetosControle_De_Vendas.br.com.projetos.dao
 {
     public class ClienteDAO
     {
+        // Atributo
+        private MySqlConnection connection;
 
+        public ClienteDAO()
+        {
+            this.connection =  new ConnectionFactory().getConnection();
+        }
+
+        #region CadastrarCliente
+        /// <summary>
+        /// Método de cadastro de Cliente
+        /// </summary>
+        public void CadastrarCliente(Clientes obj)
+        {
+            try
+            {
+                // 1 passo - define the cmd sql = insert into
+                string cmdSql = @"insert into tb_clientes (nome,rg,cpf,email,telefone,celular,endereco,numero,complemento,bairro,cidade,estado)
+values(@nome,@rg,@cpf,@email,@telefone,@celular,@endereco,@numero,@complemento,@bairro,@cidade,@estado");
+
+                //2 passo - Transcribe the commands of SQL to CSharp
+                MySqlCommand sqlCmd = new MySqlCommand(cmdSql, connection);
+                sqlCmd.Parameters.AddWithValue("@nome", obj.Nome);
+                sqlCmd.Parameters.AddWithValue("@rg", obj.RG);
+                sqlCmd.Parameters.AddWithValue("@cpf", obj.CPF);
+                sqlCmd.Parameters.AddWithValue("@email", obj.Email);
+                sqlCmd.Parameters.AddWithValue("@telefone", obj.Telefone);
+                sqlCmd.Parameters.AddWithValue("@celular", obj.Celular);
+                sqlCmd.Parameters.AddWithValue("@endereco", obj.Endereco);
+                sqlCmd.Parameters.AddWithValue("@numero", obj.Numero);
+                sqlCmd.Parameters.AddWithValue("@complemento", obj.Complemento);
+                sqlCmd.Parameters.AddWithValue("@bairro", obj.Bairro);
+                sqlCmd.Parameters.AddWithValue("@cidade", obj.Cidade);
+                sqlCmd.Parameters.AddWithValue("@estado", obj.Estado);
+
+                // 3 passo - Opening the connection and execulte the command SQL
+                connection.Open();
+
+                string resp = sqlCmd.ExecuteNonQuery() == 1 ? "O cliente foi cadastrado com sucesso" : "O cleinte não foi cadastrado"; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Aconteceu um erro! Erro do tipo: {ex.Message} com o caminho: {ex.Message}");
+            }
+            finally
+            {
+
+            }
+        }
+        #endregion
     }
+    
 }
