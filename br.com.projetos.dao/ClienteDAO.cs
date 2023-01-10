bc824c8e99b85b9543d9c5557f28646d67035597
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-01-10 05:58:13 -0300

    Criação dos métodos EdiarCleinte e ExcluirCliente

--- br.com.projetos.dao/ClienteDAO.cs
+++ br.com.projetos.dao/ClienteDAO.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.SqlServer.Server;
 using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Bcpg;
 using ProjetosControle_De_Vendas.br.com.projetos.conexao;
 using ProjetosControle_De_Vendas.br.com.projetos.model;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -65,7 +69,7 @@ values(@nome,@rg,@cpf,@email,@telefone,@celular,@endereco,@numero,@complemento,@
         #endregion
 
         #region ConsultarClientes
-        public DataTable ConsultaClientes()
+        public DataTable ConsultarClientes()
         {
             try
             {
@@ -88,6 +92,68 @@ values(@nome,@rg,@cpf,@email,@telefone,@celular,@endereco,@numero,@complemento,@
             {
                 MessageBox.Show($"Aconteceu um erro: {ex.StackTrace}");
                 return null;
+            }
+        }
+
+        #endregion
+
+        #region EditarCliente
+
+        public void EditarCliente(Clientes cliente)
+        {
+            try
+            {
+
+                string sqlcmd = @"update db.vendas.tb.clientes set nome=@nome,rg=@rg,cpf=@cpf,email=@email,telefone=@telefone,celular=@celular,cep=@cep,endereco=@endereco,numero=@numero,
+complemento=@complemento,bairro=@bairro,cidade=@cidade,estado=@estado
+where id=@id";
+                MySqlCommand cmdSql = new MySqlCommand(sqlcmd,connection);
+                cmdSql.Parameters.AddWithValue("@nome",cliente.Nome);
+                cmdSql.Parameters.AddWithValue("@rg",cliente.RG);
+                cmdSql.Parameters.AddWithValue("@cpf",cliente.CPF);
+                cmdSql.Parameters.AddWithValue("@email",cliente.Email);
+                cmdSql.Parameters.AddWithValue("@telefone",cliente.Telefone);
+                cmdSql.Parameters.AddWithValue("@celular",cliente.Celular);
+                cmdSql.Parameters.AddWithValue("@cep",cliente.CEP);
+                cmdSql.Parameters.AddWithValue("@endereco",cliente.Endereco);
+                cmdSql.Parameters.AddWithValue("@numero",cliente.Numero);
+                cmdSql.Parameters.AddWithValue("@complemento",cliente.Complemento);
+                cmdSql.Parameters.AddWithValue("@bairro",cliente.Bairro);
+                cmdSql.Parameters.AddWithValue("@cidade",cliente.Cidade);
+                cmdSql.Parameters.AddWithValue("@estado",cliente.Estado);
+                cmdSql.Parameters.AddWithValue("@id",cliente.Codigo);
+
+                connection.Open();
+                cmdSql.ExecuteNonQuery();
+
+                MessageBox.Show($"O Cliente {cliente.Nome} foi editado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"O cliente {cliente.Nome} não foi editado com sucesso!: Aconteceu um erro do tipo {ex.Message} com o caminho {ex.StackTrace}");
+            }
+        }
+        #endregion
+
+        #region ExcluirClientes
+
+        public void ExcluirCleinte(Clientes cliente)
+        {
+            try
+            {
+                string cmdSql = "delete from db.vendas.tb.clietes where id=@id";
+
+                MySqlCommand sqlCmd = new MySqlCommand(cmdSql,connection);
+                sqlCmd.Parameters.AddWithValue("@id",cliente.Codigo);
+
+                connection.Open();
+                sqlCmd.ExecuteNonQuery();
+
+                MessageBox.Show($"O cliente {cliente.Nome} foi excluido com sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"A exclusão do cliente {cliente.Nome} não foi bem sucedida, mensagem de erro: {ex.Message}, Caminho: {ex.StackTrace}");
             }
         }
 
