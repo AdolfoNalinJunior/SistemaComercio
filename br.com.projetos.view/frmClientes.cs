5c89960b26f3566abea905a7a7b205f58efddad9
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-01-19 05:53:42 -0300

    Corrigindo o erro que estava acontecendo no cep

--- br.com.projetos.view/frmClientes.cs
+++ br.com.projetos.view/frmClientes.cs
@@ -183,5 +183,28 @@ namespace ProjetosControle_De_Vendas.br.com.projetos.view
                 dgListaClientes.DataSource = clienteDao.ConsultarClientes();
             }
         }
+
+        private void btnPesquisarCEP_Click(object sender, EventArgs e)
+        {
+            // Botão consultar 
+            try
+            {
+                string cep = mtbCEP.Text;
+                string xmlUrl = "https://viacep.com.br/ws/"+cep+"/xml";
+                DataSet dado = new DataSet();
+                dado.ReadXml(xmlUrl);
+
+                txtEndereco.Text = dado.Tables[0].Rows[0]["logradouro"].ToString();
+                txtBairro.Text = dado.Tables[0].Rows[0]["bairro"].ToString();
+                txtCidade.Text = dado.Tables[0].Rows[0]["localidade"].ToString();
+                txtComplemento.Text = dado.Tables[0].Rows[0]["complemento"].ToString();
+                cbEstado.Text = dado.Tables[0].Rows[0]["uf"].ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Endereço não encontrado. Por favor digite manualmente!");
+            }
+        }
     }
 }
