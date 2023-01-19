37c2fad822cba43d560cdc4f1f68bf56397e5208
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-01-19 06:36:38 -0300

    Classe deticada para métodos de emergencia: Criando o método LimparTela

---
+++ br.com.projetos.model/Helpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetosControle_De_Vendas.br.com.projetos.model
+{
+    public class Helpers
+    {
+        #region Limpar
+        public void LimparTela(Form tela)
+        {
+            foreach (Control ctrPai in tela.Controls)
+            {
+                foreach (Control ctr1 in ctrPai.Controls)
+                {
+                    if (ctr1 is TabPage)
+                    {
+                        foreach (Control ctr2 in ctr1.Controls)
+                        {
+                            if (ctr2 is TextBox)
+                            {
+                                (ctr2 as TextBox).Text = String.Empty;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+}
