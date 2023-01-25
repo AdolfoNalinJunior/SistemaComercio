0b41b4bddfad4c3b250c95515196cf12b96b8450
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-01-25 06:30:51 -0300

    Adicionando o o método bntSalvar_Click

--- br.com.projetos.view/frmFuncionario.cs
+++ br.com.projetos.view/frmFuncionario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjetosControle_De_Vendas.br.com.projetos.dao;
+using ProjetosControle_De_Vendas.br.com.projetos.model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,5 +23,39 @@ namespace ProjetosControle_De_Vendas.br.com.projetos.view
         {
 
         }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #region BotaoSalvar
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            funcionario.Nome = txtNome.Text;
+            funcionario.RG = mtbRG.Text;
+            funcionario.CPF= mtbCPF.Text;
+            funcionario.Email = txtEmail.Text;
+            funcionario.Senha = txtSenha.Text;
+            funcionario.Cargo = cbCargo.Text;
+            funcionario.NivelAcesso = cbNivelAcesso.Text;
+            funcionario.Telefone = mtbTelefone.Text;
+            funcionario.Celular = mtbCelular.Text;
+            funcionario.CEP = mtbCEP.Text;
+            funcionario.Endereco = txtEndereco.Text;
+            funcionario.Numero = Convert.ToInt32(txtNumero.Text);
+            funcionario.Complemento = txtComplemento.Text;
+            funcionario.Bairro = txtBairro.Text;
+            funcionario.Cidade = txtCidade.Text;
+            funcionario.Estado = cbEstado.Text;
+
+            FuncionarioDAO objDao = new FuncionarioDAO();
+            objDao.CadastrarFuncionario(funcionario);
+
+            //dgListaFuncionario.DataSource = objDao.ConsultarFuncionario();
+        }
+        #endregion
     }
 }
