330031c39f1d006a0175909f50451df22cc25388
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-02-06 20:10:41 -0300

    Passando atributos por herança e criação do atributo cnpj

---
+++ br.com.projetos.model/Fornecedores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosControle_De_Vendas.br.com.projetos.model
+{
+    internal class Fornecedores
+    {
+    }
+}
