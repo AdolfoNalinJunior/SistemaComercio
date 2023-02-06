07f1c181430d6a58ce4e662a774757dfebe29a9f
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-02-06 20:19:32 -0300

    Criando atributos da conexão e construtor com a conexão encorporada nela

---
+++ br.com.projetos.dao/FornecedoresDAO.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetosControle_De_Vendas.br.com.projetos.conexao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosControle_De_Vendas.br.com.projetos.dao
+{
+    public class FornecedoresDAO
+    {
+        #region AtributosClasse
+        private MySqlConnection connection;
+        #endregion
+
+        #region Construtor
+        public FornecedoresDAO()
+        {
+            this.connection = new ConnectionFactory().getConnection();
+        }
+        #endregion
+
+    }
+}
