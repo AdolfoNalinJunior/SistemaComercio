6ad935173e93087d318dd657bac79bc92908cd3f
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-02-06 20:28:15 -0300

    Adicionando o método CadastrarFornecedor

--- br.com.projetos.dao/FornecedoresDAO.cs
+++ br.com.projetos.dao/FornecedoresDAO.cs
@@ -1,10 +1,12 @@
 ﻿using MySql.Data.MySqlClient;
 using ProjetosControle_De_Vendas.br.com.projetos.conexao;
+using ProjetosControle_De_Vendas.br.com.projetos.model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ProjetosControle_De_Vendas.br.com.projetos.dao
 {
@@ -14,12 +16,50 @@ namespace ProjetosControle_De_Vendas.br.com.projetos.dao
         private MySqlConnection connection;
         #endregion
 
-        #region Construtor
+        #region FornecedorDAO
         public FornecedoresDAO()
         {
             this.connection = new ConnectionFactory().getConnection();
         }
         #endregion
 
+        #region CadastrarFonecedor
+        public void CadastrarFornecedor(Fornecedores obj)
+        {
+            try
+            {
+                // 1 passo - define the cmd sql = insert into
+                string cmdSql = @"insert into tb_fornecedores (nome,cnpf,email,telefone,celular,endereco,numero,complemento,bairro,cidade,estado)
+values(@nome,@cnpf,@email,@telefone,@celular,@endereco,@numero,@complemento,@bairro,@cidade,@estado)";
+
+                //2 passo - Transcribe the commands of SQL to CSharp
+                MySqlCommand sqlCmd = new MySqlCommand(cmdSql, connection);
+                sqlCmd.Parameters.AddWithValue("@nome", obj.Nome);
+                sqlCmd.Parameters.AddWithValue("@cnpj", obj.CNPJ);
+                sqlCmd.Parameters.AddWithValue("@email", obj.Email);
+                sqlCmd.Parameters.AddWithValue("@telefone", obj.Telefone);
+                sqlCmd.Parameters.AddWithValue("@celular", obj.Celular);
+                sqlCmd.Parameters.AddWithValue("@endereco", obj.Endereco);
+                sqlCmd.Parameters.AddWithValue("@numero", obj.Numero);
+                sqlCmd.Parameters.AddWithValue("@complemento", obj.Complemento);
+                sqlCmd.Parameters.AddWithValue("@bairro", obj.Bairro);
+                sqlCmd.Parameters.AddWithValue("@cidade", obj.Cidade);
+                sqlCmd.Parameters.AddWithValue("@estado", obj.Estado);
+
+                // 3 passo - Opening the connection and execulte the command SQL
+                this.connection.Open();
+
+                sqlCmd.ExecuteNonQuery();
+
+                MessageBox.Show($"O fornecedor {obj.Nome} foi cadastrado com sucesso!");
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Aconteceu um erro! Erro do tipo: {ex.Message} com o caminho: {ex.StackTrace}");
+            }
+        }
+        #endregion
     }
 }
