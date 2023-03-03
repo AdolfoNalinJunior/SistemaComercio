3227a5091b2f2e77fd621ec67c80a66270817d45
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-03-03 05:47:56 -0300

    Criando os campos da classe vendas

---
+++ br.com.projetos.model/Vendas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosControle_De_Vendas.br.com.projetos.model
+{
+    public class Vendas
+    {
+        public int Codigo { get; set; }
+        public int CodigoCliente { get; set; }
+        public DateTime Data { get; set; }
+        public double TotalVenda { get; set; }
+        public string Observacao { get; set; }
+    }
+}
