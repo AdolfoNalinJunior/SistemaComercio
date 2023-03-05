bb063021fbe89b85cb4f9c5bb7d3d70df660b404
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-03-05 15:28:56 -0300

    Criando evento txtCodigo_KeyPress

--- br.com.projetos.view/frmVendas.cs
+++ br.com.projetos.view/frmVendas.cs
@@ -19,18 +19,34 @@ namespace ProjetosControle_De_Vendas.br.com.projetos.view
             InitializeComponent();
         }
 
-        #region Objs
-        Clientes obj = new Clientes();
-        ClienteDAO objDAO = new ClienteDAO();
-        #endregion
-
         #region mtbCPF_KeyPress
         private void mtbCPF_KeyPress(object sender, KeyPressEventArgs e)
         {
+            Clientes obj = new Clientes();
+            ClienteDAO objDAO = new ClienteDAO();
             if (e.KeyChar == 13)
             {
                 obj = objDAO.BuscarClienteVendas(mtbCPF.Text);
                 txtNomeCliente.Text = obj.Nome;
+            }
+        }
+
+        #endregion
+
+        #region txtCodigo_KeyPress
+        private void txtCodigo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Produtos obj = new Produtos();
+            ProdutosDAO objDAO = new ProdutosDAO();
+
+            if (e.KeyChar == 13)
+            {
+                obj = objDAO.BuscarProdutoVenda(int.Parse(txtCodigo.Text));
+
+                txtCodigo.Text = obj.Codigo.ToString();
+                txtDescricao.Text = obj.Descricao;
+                txtEstoque.Text = obj.Estoque.ToString();
+                txtValor.Text = obj.Preco.ToString();
             }
         }
         #endregion
