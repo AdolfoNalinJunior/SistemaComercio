4c4b36d1816c67edb41d51a9681f1606128cf1d2
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-03-05 13:29:37 -0300

    Criando os Contrutores de Clientee e ClienteDAO e o evento mtbCPF_KeyPress

---
+++ br.com.projetos.view/frmVendas.cs
@@ -0,0 +1,38 @@
+﻿using ProjetosControle_De_Vendas.br.com.projetos.dao;
+using ProjetosControle_De_Vendas.br.com.projetos.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetosControle_De_Vendas.br.com.projetos.view
+{
+    public partial class frmVendas : Form
+    {
+        public frmVendas()
+        {
+            InitializeComponent();
+        }
+
+        #region Objs
+        Clientes obj = new Clientes();
+        ClienteDAO objDAO = new ClienteDAO();
+        #endregion
+
+        #region mtbCPF_KeyPress
+        private void mtbCPF_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                obj = objDAO.BuscarClienteVendas(mtbCPF.Text);
+                txtNomeCliente.Text = obj.Nome;
+            }
+        }
+        #endregion
+    }
+}
