f7543deaba377e69f39ba7ded870ff20cae25224
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-03-11 17:14:14 -0300

    Adicionando comentários

--- br.com.projetos.model/Vendas.cs
+++ br.com.projetos.model/Vendas.cs
@@ -8,10 +8,16 @@ namespace ProjetosControle_De_Vendas.br.com.projetos.model
 {
     public class Vendas
     {
+        #region Campos da classe
+        /// <summary>
+        /// Campos da calsse
+        /// </summary>
         public int Codigo { get; set; }
         public int CodigoCliente { get; set; }
         public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
         public double TotalVenda { get; set; }
         public string Observacao { get; set; }
+        #endregion
     }
 }
