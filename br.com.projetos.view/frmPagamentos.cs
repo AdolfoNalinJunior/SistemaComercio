5785483acd72b408998aeaea4993856f3e67b2d7
Adolfo Nalin Junuior <adolfonalin22@gmail.com>
2023-03-15 04:31:55 -0300

    Modificando as variáveis do construtor. Criando evento load com as configurações de inicio da tela. Criando evento btnFV_Click

--- br.com.projetos.view/frmPagamentos.cs
+++ br.com.projetos.view/frmPagamentos.cs
@@ -1,8 +1,11 @@
-﻿using ProjetosControle_De_Vendas.br.com.projetos.model;
+﻿using ProjetosControle_De_Vendas.br.com.projetos.dao;
+using ProjetosControle_De_Vendas.br.com.projetos.model;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,23 +19,68 @@ namespace ProjetosControle_De_Vendas.br.com.projetos.view
         #region Componentes da classe
         Clientes obj = new Clientes();
         DataTable carrinho = new DataTable();
+        Vendas vendas = new Vendas();
         DateTime dataAtual;
         DateTime horaAtual;
         #endregion
 
-        public frmPagamentos(Clientes cliente,DataTable carrinho, DateTime dataAtual, DateTime horaAtual)
+        #region ConstrutorParametros
+        public frmPagamentos(Clientes cliente,DataTable carrinho)
         {
             this.obj = cliente;
             this.carrinho = carrinho;
-            this.dataAtual = dataAtual;
-            this.horaAtual = horaAtual;
 
             InitializeComponent();
         }
+        #endregion
 
+        #region Load
         private void frmPagamentos_Load(object sender, EventArgs e)
         {
-
+            txtDinheiro.Text = "0,00";
+            txtCartao.Text = "0,00";
+            txtTroco.Text = "0,00";
         }
+        #endregion
+
+        #region btnFV_Click
+        private void btnFV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal dinheiro, cartao, troco, totalPago, total;
+
+                dinheiro = decimal.Parse(txtDinheiro.Text);
+                cartao = decimal.Parse(txtCartao.Text);
+                total = decimal.Parse(txtTotal.Text);
+
+                totalPago = dinheiro + cartao;
+
+                if (totalPago < total)
+                {
+                    MessageBox.Show("O total pago é menor que o valor total da venda");
+                }
+                else
+                {
+                    troco = total - totalPago;
+                    txtTroco.Text = troco.ToString();
+
+                    Vendas vendas = new Vendas();
+                    vendas.CodigoCliente = obj.Codigo;
+                    vendas.Data = dataAtual;
+                    vendas.TotalVenda = total;
+                    vendas.Observacao = txtObs.Text;
+
+                    VendasDAO vDao = new VendasDAO();
+                    vDao.CadastrarVenda(vendas);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Aconteceu um erro do tipo {ex.Message} com o caminho {ex.StackTrace}");
+            }
+        }
+        #endregion
     }
 }
